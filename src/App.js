87f2af7958870784772dde7324e11d407b3e9641
nicostran <nstranquist@gmail.com>
2019-08-15 17:40:33 -0500

    add project structure, complete first todo item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// import screens
+import HomeScreen from './screens/home_screen'
+import BattleScreen from './screens/battle_screen'
+
 function App() {
   return (
     <div className="App">
-      
+      <HomeScreen />
+      <BattleScreen />
     </div>
   );
 }
